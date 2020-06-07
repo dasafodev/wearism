f31cabf56442c4c2b5d55a93c5619f9b62dba98b
Santiago Forero <ds.foreroa@uniandes.edu.co>
2020-06-07 12:53:22 -0500

    HorizontalCards have new properties

--- src/App.js
+++ src/App.js
@@ -1,50 +1,96 @@
-import React, {useState} from 'react';
-import Navbar from './components/Navbar';
-import Banner from './components/Banner';
-import HorCard from './components/HorizontalCard';
-import './App.css';
-
-
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import HorCard from "./components/HorizontalCard";
+import "./App.css";
 
 function App() {
-  
-  const [theme, setTheme] = useState('light');
+  const categories = [
+    {
+      text: "HOME",
+      icon: "home",
+      isSelected: false,
+      height: "70px",
+      width: "200px",
+    },
+    {
+      text: "NEW IN",
+      icon: "energy",
+      isSelected: true,
+      height: "70px",
+      width: "200px",
+    },
+    {
+      text: "COATS",
+      icon: "jacket",
+      isSelected: false,
+      height: "70px",
+      width: "200px",
+    },
+    {
+      text: "TOPS",
+      icon: "top",
+      isSelected: false,
+      height: "70px",
+      width: "200px",
+    },
+    {
+      text: "KNITWEAR",
+      icon: "",
+      isSelected: false,
+      height: "70px",
+      width: "200px",
+    },
+  ];
+  const [theme, setTheme] = useState("light");
 
   const verifyTheme = () => {
-    if(theme==='dark'){
-      document.documentElement.style.cssText=`
+    if (theme === "dark") {
+      document.documentElement.style.cssText = `
       --first-color:black;
       --second-color:white;
       --third-color:blue;
 
     `;
-    }
-    else{
-      document.documentElement.style.cssText=`
+    } else {
+      document.documentElement.style.cssText = `
       --first-color:white;
       --second-color:black;
       --third-color:red;
 
     `;
     }
-  }
+  };
 
   const changeTheme = () => {
-    (theme==='light')?setTheme('dark'): setTheme('light');
-  }
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
   const handleTheme = () => {
     changeTheme();
     verifyTheme();
-  }
+  };
 
   return (
-    <div  className="App">
-      <Navbar/>
-      <Banner/>
-      <HorCard isSelected={true}/>
-      <h1>Hello</h1>
-      <p>World</p>
+    <div className="App">
+      <Navbar />
+      <Banner />
+      <div className="Content">
+        <div className="Categories">
+          {categories.map((category) => {
+            return (
+              <HorCard
+                key={category.id}
+                height={category.height}
+                width={category.width}
+                text={category.text}
+                icon={category.icon}
+                isSelected={category.isSelected}
+              />
+            );
+          })}
+        </div>
+      </div>
       <button onClick={handleTheme}>Change Theme</button>
     </div>
   );
--- src/components/HorizontalCard.js
+++ src/components/HorizontalCard.js
@@ -1,45 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // import './styles/HorizontalCard.css';
-import lightning from './../assets/black-energy.png';
+// import lightning from './../assets/black-energy.png';
 
 function HorizontalCard(props) {
 
-    const [image, setImage] = useState(lightning);
+    const [image, setImage] = useState(null);
 
-    const cardStyles = {
+    let cardStyles = {
         display: 'flex',
         color: 'black',
-        'backgroundColor': 'var(--element-gray)',
-        'alignItems': 'center',
-        'justifyContent': 'space-around',
-        'width': '220px',
-        'height': '70px',
-        'borderRadius': '15px'
+        backgroundColor: 'var(--element-gray)',
+        alignItems: 'center',
+        padding: '0 20px',
+        justifyContent: 'space-between',
+        width: props.width,
+        height: props.height,
+        borderRadius: '15px',
+        margin: '15px 0'
     }
-    // useEffect(() => {
-    //     let title = React.createRef();
-    //     console.log(title)
-    //     const selected = (isSelected) => {
 
-    //         if (isSelected && title.current) {
-    //             title.current.style.cssText = `
-    //                 color: white;
-    //                 background-color: rgb(154, 102, 225);
-    //                 `;
-    //             import(`./../assets/white-energy.png`).then(resp => {
-    //                 setImage(resp.default);
-    //             })
-    //         }
+    const selected =  (isSelected) => {
+        if (isSelected) {
+            cardStyles.color = 'white';
+            cardStyles.backgroundColor = 'rgb(154, 102, 225)';
+        }
+        if(props.icon){
+            assignIcon(isSelected,props.icon)
+        }
+        else{
+            cardStyles.justifyContent = 'center'
 
-    //     }
+        }
+       
+    }
 
-    //     selected(props.isSelected);
-    // })
+    const assignIcon = (isSelected,icon) =>{
+        if(isSelected)
+        {
+            import(`./../assets/white-${icon}.png`).then(resp => {
+                setImage(resp.default);
+            })
+    
+        }
+        else{
+            import(`./../assets/black-${icon}.png`).then(resp => {
+                setImage(resp.default);
+            })
+        }
+    }
+
+    selected(props.isSelected);
 
     return (
 
         <div style={cardStyles} className="Card">
-            <span>Hola</span>
+            <span>{props.text}</span>
             <img src={image} alt="" />
         </div>
     )
